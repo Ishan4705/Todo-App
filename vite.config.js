7be3c25265d54aa163c904e196c176118e09cad1
Ishan Mittal <ishanm.2205@gmail.com>
2024-10-02 15:44:10 +0530

    some vite.config.js changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,  // or any other port you want to use
+    host: '0.0.0.0', // Allows deployment to listen on all network interfaces
+  },
+  build: {
+    outDir: 'dist', // Make sure this matches your Vercel build config
+  },
+});
