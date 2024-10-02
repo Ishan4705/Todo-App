5cf79a0f7e448e852081c7933ad8849c3fc7facd
Ishan Mittal <ishanm.2205@gmail.com>
2024-10-02 11:02:40 +0530

    task completion line-through animation added

--- src/Task.jsx
+++ src/Task.jsx
@@ -2,9 +2,9 @@ import CheckBox from "./CheckBox";
 
 export default function Task({name,done,onToggle}){
     return(
-        <div className="task">
+        <div className={'task ' + (done ? 'done' : '' )}>
             <CheckBox checked={done} onClick={()=>{onToggle(!done)}}/>
-          {name}
+          <span>{name}</span>
         </div>
     );
 }
